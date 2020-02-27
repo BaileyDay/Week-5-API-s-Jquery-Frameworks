a09c88b0a008257eef279d35a93274cfeef37e2e
Bailey Day <bailey.j.day@gmail.com>
2020-02-27 10:52:20 -0500

    Refactored for use with asynchronous functions

--- Chuck Norris API test/js/get.js
+++ Chuck Norris API test/js/get.js
@@ -2,13 +2,20 @@
 
 function get(url) {
   return fetch(url)
-    .then(function(response) {
+    .then(function (response) {
       return response.json();
     })
-    .then(function(data) {
+    .then(function (data) {
       return data;
     })
-    .catch(function(error) {
+    .catch(function (error) {
       return error;
     });
+}
+
+
+const getWithAwait = async url => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 }
--- Chuck Norris API test/js/scripts.js
+++ Chuck Norris API test/js/scripts.js
@@ -7,46 +7,45 @@ const submitFormButton = document.querySelector('#submitForm');
 const categoryChangeForm = document.querySelector('#categoryChangeForm');
 const closeModalButton = document.querySelector('#closeModal');
 
-function getQuote(category) {
+const getQuote = async category => {
   const apiUrl = `https://api.chucknorris.io/jokes/random?category=${category}`;
   const chuckSaysParagraph = document.querySelector('#chuckSays');
   const modalWindow = document.querySelector(".modal-overlay");
 
-  get(apiUrl).then(function(response) {
-    chuckSaysParagraph.innerHTML = response.value;
-    modalWindow.classList.toggle("open");
-  });
+  const theQuote = await getWithAwait(apiUrl);
+  chuckSaysParagraph.innerHTML = theQuote.value
+  modalWindow.classList.toggle('open');
 }
 
-function getCategories() {
+
+
+const getCategories = async () => {
   const apiUrl = `https://api.chucknorris.io/jokes/categories`;
   const categorySelectLabel = document.querySelector('#categorySelectLabel');
 
-  get(apiUrl).then(function(response) {
-    const categoryList = response.filter(function(category) {
-      if (category != 'explicit') {
-        return category;
-      }
-    });
-    // Create a select element for our categories
-    const categoryElement = document.createElement('select');
-    // Create the options for the select element
-    categoryList.map(function(category) {
-      const categoryOption = document.createElement('option');
-      categoryOption.value = category;
-      categoryOption.text = category;
-      categoryElement.append(categoryOption);
-    });
-    categorySelectLabel.appendChild(categoryElement);
+  const categoryList = await getWithAwait(apiUrl);
+
+  const categoryElement = document.createElement('select');
+  // Create the options for the select element
+  categoryList.map(function (category) {
+    const categoryOption = document.createElement('option');
+    categoryOption.value = category;
+    categoryOption.text = category;
+    categoryElement.append(categoryOption);
   });
+  categorySelectLabel.appendChild(categoryElement);
+
+
+
+
 }
 
-refreshQuoteButton.addEventListener('click', function(e) {
+refreshQuoteButton.addEventListener('click', function (e) {
   e.preventDefault();
   getQuote(category);
 });
 
-submitFormButton.addEventListener('click', function(e) {
+submitFormButton.addEventListener('click', function (e) {
   e.preventDefault();
   const categoryInput = document.querySelector('#categoryChangeForm select');
 
@@ -54,9 +53,11 @@ submitFormButton.addEventListener('click', function(e) {
   getQuote(category);
 });
 
-closeModalButton.addEventListener('click', function(e) {
+closeModalButton.addEventListener('click', function (e) {
   const modalWindow = document.querySelector('.modal-overlay');
   modalWindow.classList.toggle('open');
 });
 getQuote(category);
 getCategories();
+
+getWithAwait();
